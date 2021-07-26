6e03498340f0865deadb744f93cd463266a27cb8
Shirly <shirlyd@axioned.com>
2021-07-26 18:44:45 +0530

    js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,20 @@
 
 */
 
+function required() 
+{
+    var input = document.forms["form"]["fname"].value;
+    if (input == "")
+    {
+        alert("Fillm all fields");
+        return false;
+    }
+    else
+    {
+        alert("You can proceed");
+        return true;
+    }
+}
 
 
 
