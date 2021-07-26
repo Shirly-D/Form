be1f191fa9b4b633db9940b7b8fc8f829b29d2dc
Shirly <shirlyd@axioned.com>
2021-07-26 20:17:19 +0530

    phone words commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,75 @@
 
 */
 
-function required() 
+// function required() 
+// {
+//     var input = document.forms["forms"]["fname"].value;
+
+//     if (input == "")
+//     {
+//         alert("Fillm all fields");
+//         return false;
+//     }
+//     else
+//     {
+//         alert("You can proceed");
+//         return true;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Print all possible words from phone number digits.
+// For example if input number is 234, possible words which can be formed are (Alphabetical order):
+// adg adh adi aeg aeh aei afg afh afi bdg bdh bdi beg beh bei bfg bfh bfi cdg cdh cdi ceg ceh cei cfg cfh cfi
+
+var mobile = [ "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" ];
+var num = [2, 3, 4];
+var n = num.length;
+
+function words (num, current_no, result, n)
 {
-    var input = document.forms["form"]["fname"].value;
-    if (input == "")
+    if (current_no == n)
     {
-        alert("Fillm all fields");
-        return false;
+        document.write(result.join("") + "<br>")
+        return;
     }
-    else
+    for (var i = 0; i < mobile[num[current_no]].length; i++)
     {
-        alert("You can proceed");
-        return true;
+        result.push(mobile[num[current_no]][i]);
+        words(num, current_no + 1, result, n);
+        result.pop();
+        if(num[current_no] == 0 || num[current_no == 1])
+        return
     }
 }
+
+function display_words(num, n)
+{
+    words(num, 0, [], n);
+}
+
+display_words(num, n);
+
+
 
 
 
