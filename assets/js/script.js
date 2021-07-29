48ef06b5526020fb382824951aa2da43e7e901bd
Shirly <shirlyd@axioned.com>
2021-07-30 00:48:11 +0530

    js table

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,45 +2,76 @@
 
 */
 
+var form = document.getElementById('form');
+form.addEventListener('submit', formSubmit);
+var detail = [];
+
 function formSubmit(event) {
     event.preventDefault();
-
     var fname = document.getElementById('fname').value;
-    console.log(fname);
     var lname = document.getElementById('lname').value;
-    console.log(lname);
-    var gender = document.getElementsByName('rad_btn');
-    for (var i = 0; i < gender.length; i++)
-    {
-        if (gender[i].checked)
-        {
-            document.getElementById('male').value;
-            document.getElementById('female').value;
-        }
-    }
-    console.log(gender);
+    var male = document.getElementById('male').value;
+    var female = document.getElementById('female').value;
+    var gender = male ? male : female ? female : "";
     var address = document.getElementById('address').value;
-    console.log(address);
-    var check = document.getElementById('check').value;
-    console.log(check);
+    var check = document.getElementById('check').checked;
+
+    if (fname != "" && lname != "" && gender != "" && address != "" && check != "")
+    {
+        alert('Thank you...!');
+        console.log(fname, lname, gender, address, check);
+
+        var obj = {
+            fname : fname,
+            lname : lname,
+            gender : gender,
+            address : address
+        }
+        detail.push(obj);
+        // console.log(detail);
+        display();
+
+    }
+    else
+    {
+        alert('Please fill all fields..');
+    }
+
+
+}
+
+function display() {
+    var table = document.createElement('table');
+    var thRow = document.createElement('tr');
+    var thName = document.createElement('th');
+    thName.textContent="Name";
+    var thLname = document.createElement('th');
+    var thGen = document.createElement('th');
+    var thAddress = document.createElement('th');
+    var thEdit = document.createElement('th');
+    var thDelete = document.createElement('th');
+     thRow.appendChild(thName);
+    table.appendChild(thRow);
+
+
+    for (var i = 0; i < detail.length; i++){
+        var tr = document.createElement('tr');
+        var fname = document.createElement('td');
+        fname.textContent=detail[i].fname;
+        tr.appendChild(fname);
+        lname = detail[i].lname;
+        gender = detail[i].gender;
+        address= detail[i].address;
+        table.appendChild(tr);
+    }
+    document.getElementsByTagName('body')[0].appendChild(table);
 }
 
 
-// function required() 
-// {
-//     var input = document.forms["forms"]["fname"].value;
-    
-//     if (input == "")
-//     {
-//         alert("Fillm all fields");
-//         return false;
-//     }
-//     else
-//     {
-//         alert("You can proceed");
-//         return true;
-//     }
-// }
+
+
+
+
 
 
 // function cancel() {
@@ -56,13 +87,6 @@ function formSubmit(event) {
 //     document.getElementById("cancel").innerHTML = a;
 // }
 
-// var table = document.createElement('table');
-// var thead = document.createElement('thead');
-// var tbody = document.createElement('tbody');
-
-// table.appendChild(thead);
-// table.appendChild(tbody);
-// document.getElementsById(body).appendChild(table);
 
 
 
@@ -83,37 +107,9 @@ function formSubmit(event) {
 
 
 
-// Print all possible words from phone number digits.
-// For example if input number is 234, possible words which can be formed are (Alphabetical order):
-// adg adh adi aeg aeh aei afg afh afi bdg bdh bdi beg beh bei bfg bfh bfi cdg cdh cdi ceg ceh cei cfg cfh cfi
 
-// var mobile = [ "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" ];
-// var num = [5, 3, 2, 6, ];
-// var n = num.length;
 
-// function words (num, current_no, result, n)
-// {
-//     if (current_no == n)
-//     {
-//         document.write(result.join("") + "<br>")
-//         return;
-//     }
-//     for (var i = 0; i < mobile[num[current_no]].length; i++)
-//     {
-//         result.push(mobile[num[current_no]][i]);
-//         words(num, current_no + 1, result, n);
-//         result.pop();
-//         if(num[current_no] == 0 || num[current_no == 1])
-//         return
-//     }
-// }
 
-// function display_words(num, n)
-// {
-//     words(num, 0, [], n);
-// }
-
-// display_words(num, n);
 
 
 
